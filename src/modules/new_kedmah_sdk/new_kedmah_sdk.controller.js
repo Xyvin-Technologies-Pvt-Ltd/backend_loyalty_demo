096fe511186c98bbb4c0f70388d841a9b4d25e19
xyvintech <admin@xyvin.com>
2025-08-19 12:07:30 +0530

    coupon

--- src/modules/new_kedmah_sdk/new_kedmah_sdk.controller.js
+++ src/modules/new_kedmah_sdk/new_kedmah_sdk.controller.js
@@ -1397,7 +1397,7 @@ const getMerchantOffers = async (req, res) => {
 
     let allCoupons = await CouponCode.find(filter)
       .populate("merchantId")
-      .sort({ priority: 1 });
+      .sort({ priority: -1 });
 
     // Replace URLs
     allCoupons.forEach((coupon) => {
